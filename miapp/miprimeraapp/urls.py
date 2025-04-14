c720b0017a04ae4bd182e0bd064c314965d4d403
Juan Diego Ramirez <jramir71@gmail.com>
2025-04-14 16:57:18 -0500

    se actualiza urls.py en archivo miprimeraapp

--- miapp/miprimeraapp/urls.py
+++ miapp/miprimeraapp/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),  # ejemplo de vista principal
+    path('insertar_categoria/', views.insertar_categoria, name='insertar_categoria'),
+    path('insertar_autor/', views.insertar_autor, name='insertar_autor'),
+    path('insertar_post/', views.insertar_post, name='insertar_post'),
+    path('buscar_post/', views.buscar_post, name='buscar_post'),
 ]
